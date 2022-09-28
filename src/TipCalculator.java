8ee58e793c848ce7f46e750eadf13d5e18421e11
Wafa Berri <wberri13@gmail.com>
2022-09-28 18:26:26 -0400

    final 3.0

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -25,24 +25,27 @@ public class TipCalculator {
         tipPercentage =  scan.nextInt();
 
         //adding up total and asking user costs.
-        while (itemCost != -1) {
+        while (itemCost != -1) { //not equal -1 because that symbolizes the user is done inputting values.
             System.out.print("Enter the cost of an item you bought in dollars and cents, e.g. 12.50(type -1 to end): ");
             itemCost = scan.nextDouble();
             totalCost += itemCost;
         }
         totalCost++; //adding the -1 that was taken away
-        System.out.println("-----------------------------------------------------------------------");
 
         totalCost *= 100; //changing to only cents
 
         //Calculations
-        tipAsDecimal = tipPercentage *0.01;
-        totalTip = totalCost * tipAsDecimal;
-        totalBillWithTip = totalCost + totalTip;
-        perPersonCostBeforeTip = totalCost / numOfPeople;
-        tipPerPerson = totalTip / numOfPeople;
-        totalCostPerPerson = tipPerPerson + perPersonCostBeforeTip;
+        tipAsDecimal = tipPercentage *0.01; //user inputs percentage as a whole number, to use, i  must change it to a decimal.
+        totalTip = totalCost * tipAsDecimal; //finds the tip
+        totalBillWithTip = totalCost + totalTip; //adds the tip to cost
+        perPersonCostBeforeTip = totalCost / numOfPeople; //divides total cost by people
+        tipPerPerson = totalTip / numOfPeople; //tip divided by ppl
+        totalCostPerPerson = tipPerPerson + perPersonCostBeforeTip; //adds the per person values
 
+        //PRINTS THE RECEIPT
+        //formatter.format is used for all values except tip percentage because they all involve dollars and cents and money is always rounded to the second decimal place.
+        //all values except tipPercentage are multiplied by 0.01 because they must be in dollars and cents, not just cents like they were before.
+        System.out.println("-----------------------------------------------------------------------");
         System.out.println("Total Bill Before Tip: " + formatter.format(totalCost*0.01));
         System.out.println("Tip Percentage: " + tipPercentage);
         System.out.println("Total Tip: " + formatter.format(totalTip*0.01));
